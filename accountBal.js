3e91a075118a95ad5884dce55fbaef1ca370565f
JeanneEricksonCooley <cool0020@gmail.com>
2015-05-06 15:49:18 -0500

    adds account balance functionality

---
+++ accountBal.js
@@ -0,0 +1,14 @@
+var convert = require('./convertUSD');
+
+function showMoney() {
+    return convert();
+}
+
+function balance() {
+    return "Account balance: \n";
+}
+
+
+
+module.exports.money = showMoney;
+module.exports.balance = balance;
--- app.js
+++ app.js
@@ -1,14 +1,31 @@
+
 var express = require('express');
 var app = express();
 
-var convert = require('./convertUSD');
+var accountBal = require("./accountBal");
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-   response.sendStatus(convert());
+   response.send(accountBal.balance() + accountBal.money());
 });
 
 app.listen(app.get('port'), function() {
    console.log("Node app running on port: ", app.get('port'));
 });
+
+
+//var express = require('express');
+//var app = express();
+//
+//var randomNum = require("./randomNum");
+//
+//app.set('port', (process.env.PORT || 5000));
+//
+//app.get('/', function(request, response) {
+//    response.send(randomNum());
+//});
+//
+//app.listen(app.get('port'), function() {
+//    console.log("Node app running on port: ", app.get('port'));
+//});
