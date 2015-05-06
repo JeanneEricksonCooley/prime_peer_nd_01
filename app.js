3de8a60233076a27e3ba0535df1e1f092da43480
JeanneEricksonCooley <cool0020@gmail.com>
2015-05-06 16:00:30 -0500

    Kitteh

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 
-var express = require('express');
-var app = express();
-
-var accountBal = require("./accountBal");
-
-app.set('port', (process.env.PORT || 5000));
-
-app.get('/', function(request, response) {
-   response.send(accountBal.balance() + accountBal.money());
-});
-
-app.listen(app.get('port'), function() {
-   console.log("Node app running on port: ", app.get('port'));
-});
-
-
 //var express = require('express');
 //var app = express();
 //
-//var randomNum = require("./randomNum");
+//var accountBal = require("./accountBal");
 //
 //app.set('port', (process.env.PORT || 5000));
 //
 //app.get('/', function(request, response) {
-//    response.send(randomNum());
+//   response.send(accountBal.balance() + accountBal.money());
 //});
 //
 //app.listen(app.get('port'), function() {
-//    console.log("Node app running on port: ", app.get('port'));
+//   console.log("Node app running on port: ", app.get('port'));
 //});
+
+
+var express = require('express');
+var app = express();
+
+var randomNum = require("./randomNum");
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+    response.send(randomNum().toString());
+});
+
+app.listen(app.get('port'), function() {
+    console.log("Node app running on port: ", app.get('port'));
+});
