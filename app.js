cebb02cbab3d3532683179022a5385ed5302d62b
JeanneEricksonCooley <cool0020@gmail.com>
2015-05-06 14:54:26 -0500

    adds call

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.get('/', function(request, response) {
+   response.send("Root the route!")
+});
+
 app.listen(app.get('port'), function() {
    console.log("Node app running on port: ", app.get('port'));
 });
